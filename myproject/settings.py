3fec7f70c398eec4bcb45d888c76cdf95a2b5ca1
Emmy-infinity <113331587+Emmy-infinity@users.noreply.github.com>
2026-06-21 22:42:38 +0300

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -167,6 +167,7 @@ USE_TZ = True
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 MEDIA_URL = '/media/'
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
