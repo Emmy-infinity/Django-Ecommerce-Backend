aaa1e482eaf3a0e5611c6ea0f42d510058d9fe53
Emmy-infinity <113331587+Emmy-infinity@users.noreply.github.com>
2026-06-21 14:58:11 +0300

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -21,28 +21,6 @@ class Note(models.Model):
     def __str__(self):
         return self.title
 
-class Product(models.Model):
-    name=models.CharField(max_length=200)
-    price=models.DecimalField(max_digits=10,decimal_places=2)
-    description=models.TextField()
-class ProductImage(models.Model):
-    product=models.ForeignKey(Product,related_name='images',on_delete=models.CASCADE)
-    image=models.ImageField(upload_to='products')
-    order=models.IntegerField(default=0)
-class UploadedImage(models.Model):
-    image = models.ImageField(upload_to='images/' )
-    title = models.CharField(max_length=100, blank=True)
-    uploaded_at = models.DateTimeField(auto_now_add=True)
-    description=models.CharField(max_length=500,blank=True)
-    
-    
-    class Meta:
-        pass
-
-    def __str__(self):
-        return self.title or str(self.image)
-
-
 
 class SensorReading(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
