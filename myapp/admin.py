0d7db2becfbfe17e633c1a0f1fb5955a8c3a960f
Emmy-infinity <113331587+Emmy-infinity@users.noreply.github.com>
2026-06-21 15:07:23 +0300

    Update admin.py

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Note,UploadedImage,SensorReading
+from .models import Note,Photo,SensorReading
 
 # Register your models here.
 admin.site.register(Note)
-admin.site.register(UploadedImage)
+admin.site.register(Photo)
 admin.site.register(SensorReading)
