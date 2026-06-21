afc2849739b390c9c296930ea1f3075a99e6d878
Emmy-infinity <113331587+Emmy-infinity@users.noreply.github.com>
2026-06-21 14:16:00 +0300

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -2,6 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+# models.py
+from django.db import models
+from cloudinary.models import CloudinaryField
+
+class Photo(models.Model):
+    title = models.CharField(max_length=100)
+    image = CloudinaryField('image') # Handles Cloudinary upload directly
+
+
+
 class Note(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
