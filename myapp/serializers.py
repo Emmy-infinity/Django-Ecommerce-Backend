3c1e4ce1002279034786eba907cd36e97bc76ad2
Emmy-infinity <113331587+Emmy-infinity@users.noreply.github.com>
2026-08-10 01:17:05 +0300

    Refactor serializers and add UserSerializer

--- myapp/serializers.py
+++ myapp/serializers.py
@@ -1,10 +1,7 @@
-from django.db import models
-from django.contrib.auth.models import User
-from cloudinary.models import CloudinaryField
-
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import Note, SensorReading, Photo  # Import Photo model here, do not define it!
+from .models import Note, SensorReading, StockMarketReading, Photo
+
 
 class PhotoSerializer(serializers.ModelSerializer):
     image = serializers.SerializerMethodField()
@@ -16,36 +13,35 @@ class PhotoSerializer(serializers.ModelSerializer):
     def get_image(self, obj):
         if obj.image:
             url = obj.image.url
+            # Force the URL to start explicitly with https://
             if url.startswith('//'):
                 return f"https:{url}"
             return url
         return None
 
-# ... rest of your serializers ...
 
-class Note(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "password"]
+        extra_kwargs = {"password": {"write_only": True}}
 
-    def __str__(self):
-        return self.title
-
-
-class SensorReading(models.Model):
-    timestamp = models.DateTimeField(auto_now_add=True)
-    value = models.FloatField()
-
-    def __str__(self):
-        return f"{self.timestamp}: {self.value}"
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
 
 
-class StockMarketReading(models.Model):
-    # Fixed auto_created=True to auto_now_add=True
-    timestamp = models.DateTimeField(auto_now_add=True)
-    value1 = models.FloatField()
-    value2 = models.FloatField()
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = ["id", "title", "content", "created_at", "author"]
+        extra_kwargs = {"author": {"read_only": True}}
 
-    def __str__(self):
-        return f"{self.timestamp}: {self.value1} / {self.value2}"
+
+class SensorReadingSerializer(serializers.ModelSerializer):
+    x = serializers.DateTimeField(source='timestamp', format='%Y-%m-%d %H:%M:%S')
+    y = serializers.FloatField(source='value')
+
+    class Meta:
+        model = SensorReading
+        fields = ['x', 'y']
