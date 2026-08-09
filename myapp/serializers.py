f8210d6d448cbc5fe99014f456abf2180a9cfe70
Emmy-infinity <113331587+Emmy-infinity@users.noreply.github.com>
2026-08-10 01:21:04 +0300

    Add serializers for User, Note, and SensorReading

--- myapp/serializers.py
+++ myapp/serializers.py
@@ -6,6 +6,8 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Note, SensorReading, Photo  # Import Photo model here, do not define it!
 
+
+
 class PhotoSerializer(serializers.ModelSerializer):
     image = serializers.SerializerMethodField()
 
@@ -16,12 +18,38 @@ class PhotoSerializer(serializers.ModelSerializer):
     def get_image(self, obj):
         if obj.image:
             url = obj.image.url
+            # Force the URL to start explicitly with https://
             if url.startswith('//'):
                 return f"https:{url}"
             return url
         return None
 
-# ... rest of your serializers ...
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "password"]
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = ["id", "title", "content", "created_at", "author"]
+        extra_kwargs = {"author": {"read_only": True}}
+
+
+class SensorReadingSerializer(serializers.ModelSerializer):
+    x = serializers.DateTimeField(source='timestamp', format='%Y-%m-%d %H:%M:%S')
+    y = serializers.FloatField(source='value')
+
+    class Meta:
+        model = SensorReading
+        fields = ['x', 'y']
 
 
 
