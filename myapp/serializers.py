ccc0d71479d70648f6b7364b0e8d7f491a6f6dea
Emmy-infinity <113331587+Emmy-infinity@users.noreply.github.com>
2026-08-10 01:09:12 +0300

    Implement PhotoSerializer for Photo model
    Added PhotoSerializer to handle Photo model serialization.

--- myapp/serializers.py
+++ myapp/serializers.py
@@ -2,13 +2,26 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
-class Photo(models.Model):
-    # Option A: Keep CloudinaryField directly
-    image = CloudinaryField('image')
-    
-    # Option B (Recommended if using cloudinary_storage storage backend):
-    # image = models.ImageField(upload_to='photos/')
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from .models import Note, SensorReading, Photo  # Import Photo model here, do not define it!
 
+class PhotoSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Photo
+        fields = ['id', 'image']
+
+    def get_image(self, obj):
+        if obj.image:
+            url = obj.image.url
+            if url.startswith('//'):
+                return f"https:{url}"
+            return url
+        return None
+
+# ... rest of your serializers ...
 
 class Note(models.Model):
     title = models.CharField(max_length=100)
