4321d89c7a5394314b9c42d3a78f24b104944839
Emmy-infinity <113331587+Emmy-infinity@users.noreply.github.com>
2026-08-10 02:30:03 +0300

    Add initial migration for Photo model
    Create initial migration for Photo model with fields.

--- myapp/migrations/0001_initial.py
+++ myapp/migrations/0001_initial.py
@@ -1,1 +1,20 @@
+from django.db import migrations, models
+import cloudinary.models
 
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Photo',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('image', cloudinary.models.CloudinaryField(max_length=255, verbose_name='image')),
+                ('uploaded_at', models.DateTimeField(auto_now_add=True)),
+            ],
+        ),
+    ]
